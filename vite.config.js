34e184e11178f67ceacee052cabd6679b9c92be3
SellPrincess <edgaaarrr@mail.ru>
2025-12-28 14:54:20 +0500

    dxcx

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Отключаем минификацию - возможно она ломает seed
-    minify: false,
-    // Отключаем разделение чанков
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  },
-  // Фиксируем версию Three.js
-  resolve: {
-    dedupe: ['three', '@react-three/fiber', '@react-three/drei']
-  }
 })
